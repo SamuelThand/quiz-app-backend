56e9451575cdef8d3a5d1160ea39c2cf4ab7c4de
toha2102 <toha2102@student.miun.se>
2022-12-29 19:37:27 +0100

    Added post/delete routes and docs

--- models/question.ts
+++ models/question.ts
@@ -23,6 +23,20 @@ interface QuestionModel extends Model<IQuestion, {}, IQuestionMethods> {
   //  TODO är detta rätt return type? Eller ska det vara ett Query<> objekt?
   getQuestions(): Promise<IQuestion[]>;
   getQuestion(name: string): Promise<IQuestion>;
+  getQuestionsByCreator(creator: string): Promise<IQuestion[]>;
+  addQuestion(
+    creator: string,
+    name: string,
+    question: string,
+    option1: string,
+    optionX: string,
+    option2: string,
+    correctOption: string,
+    level: Number,
+    subject: string[],
+    language: string
+  ): Promise<IQuestion>;
+  deleteQuestionByName(name: string): Promise<IQuestion>;
 }
 
 const questionSchema = new Schema<IQuestion, QuestionModel, IQuestionMethods>({
@@ -33,7 +47,7 @@ const questionSchema = new Schema<IQuestion, QuestionModel, IQuestionMethods>({
   optionX: String,
   option2: String,
   correctOption: String, // Referera till en option ovan
-  date: Date,
+  date: { type: Date, default: Date.now },
   level: Number,
   subject: [{ type: String, ref: 'subjects' }],
   language: String
@@ -41,14 +55,89 @@ const questionSchema = new Schema<IQuestion, QuestionModel, IQuestionMethods>({
 
 // Statics (Model functions)
 
-//  TODO Ska denna funktion vara async?
+/**
+ * Get all questions
+ *
+ * @returns {Promise<IQuestion[]>} Promise of all questions
+ */
 questionSchema.static('getQuestions', async function () {
   return this.find({});
 });
 
-// TODO: Possibly return multiple questions?
-questionSchema.static('getQuestion', async function (name: string) {
-  return this.findOne({ name });
+// TODO: Possibly return multiple questions by name?
+
+/**
+ * Get question by id
+ *
+ * @param {string} id - Id of question
+ * @returns {Promise<IQuestion>} Promise of question
+ */
+questionSchema.static('getQuestion', async function (id: string) {
+  return this.findById(id);
+});
+
+/**
+ * Get all questions by a specific creator
+ *
+ * @param {string} creator - name of creator
+ * @returns {Promise<IQuestion[]>} Promise of all questions by the creator
+ */
+questionSchema.static(
+  'getQuestionsByCreator',
+  async function (creator: string) {
+    return this.find({ creator });
+  }
+);
+
+/**
+ * Add a new question to the database
+ *
+ * @param {string} creator - name of creator
+ * @param {string} name - name of question
+ * @param {string} question - question
+ * @param {string} option1 - first option
+ * @param {string} optionX - second option
+ * @param {string} option2 - third option
+ * @param {string} correctOption - correct option
+ * @param {number} level - level of question
+ * @param {string[]} subject - subject of question
+ * @param {string} language - language of question
+ * @returns {Promise<IQuestion>} Promise of the new question
+ */
+questionSchema.static(
+  'addQuestion',
+  async function (
+    creator: string,
+    name: string,
+    question: string,
+    option1: string,
+    optionX: string,
+    option2: string,
+    correctOption: string,
+    level: number,
+    subject: string[],
+    language: string
+  ) {
+    // TODO: Change name of questionModel to something else, possibly "Question"?
+    const newQuestion = new questionModel({
+      creator,
+      name,
+      question,
+      option1,
+      optionX,
+      option2,
+      correctOption,
+      level,
+      subject,
+      language
+    });
+
+    return await newQuestion.save();
+  }
+);
+
+questionSchema.static('deleteQuestionByName', async function (name: string) {
+  return this.findOneAndDelete({ name });
 });
 
 // Methods (Document functions)
--- routes/questions.ts
+++ routes/questions.ts
@@ -3,49 +3,92 @@ import { questionModel } from '../models/question';
 
 const questionsRoutes = Express.Router();
 
+// Return all questions
 questionsRoutes.get(
   '/',
   async function (req: Express.Request, res: Express.Response) {
-    const respon = await questionModel.getQuestions();
-    res.status(200).json(respon);
+    const result = await questionModel.getQuestions();
+    res.status(200).json(result);
   }
 );
 
 // TODO: Remove one of name/code ?
 
+// Problem with name as parameter
+// questionsRoutes.get(
+//   '/:name',
+//   async function (req: Express.Request, res: Express.Response) {
+//     const name = req.params.name;
+//     console.log(name);
+//     const result = await questionModel.getQuestion(name);
+//     res.status(200).json(result);
+//   }
+// );
+
 questionsRoutes.get(
-  '/:name',
-  function (req: Express.Request, res: Express.Response) {
-    const name = req.params.name;
-    questionModel.getQuestion(name);
+  '/:id',
+  async function (req: Express.Request, res: Express.Response) {
+    const id = req.params.id;
+    console.log(id);
+    const result = await questionModel.getQuestion(id);
+    res.status(200).json(result);
   }
 );
 
-// questionsRoutes.get(
-//   '/:code',
+questionsRoutes.post(
+  '/',
+  async function (req: Express.Request, res: Express.Response) {
+    const creator = req.body.creator;
+    const name = req.body.name;
+    const question = req.body.question;
+    const option1 = req.body.option1;
+    const optionX = req.body.optionX;
+    const option2 = req.body.option2;
+    const correctOption = req.body.correctOption;
+    // Skip date and go for default value (Date.now) instead
+    // const date = req.body.date;
+    const level = req.body.level;
+    const subject = req.body.subject;
+    const language = req.body.language;
+
+    console.log('creator: ' + creator);
+
+    const result = await questionModel.addQuestion(
+      creator,
+      name,
+      question,
+      option1,
+      optionX,
+      option2,
+      correctOption,
+      level,
+      subject,
+      language
+    );
+    res.status(200).json(result);
+  }
+);
+
+questionsRoutes.put(
+  '/:id',
+  function (req: Express.Request, res: Express.Response) {
+    throw Error('Fool');
+  }
+);
+
+// TODO: Possibly keep this one and remove the name one
+// questionsRoutes.delete(
+//   '/:id',
 //   function (req: Express.Request, res: Express.Response) {
 //     throw Error('Fool');
 //   }
 // );
 
-questionsRoutes.post(
-  '/',
-  function (req: Express.Request, res: Express.Response) {
-    throw Error('Fool');
-  }
-);
-
-questionsRoutes.put(
-  '/:code',
-  function (req: Express.Request, res: Express.Response) {
-    throw Error('Fool');
-  }
-);
-
 questionsRoutes.delete(
-  '/:code',
-  function (req: Express.Request, res: Express.Response) {
-    throw Error('Fool');
+  '/:name',
+  async function (req: Express.Request, res: Express.Response) {
+    const result = await questionModel.deleteQuestionByName(req.params.name);
+    res.status(200).json(result);
   }
 );
 
