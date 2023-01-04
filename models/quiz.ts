f02fe47f8113c536a66bc6117ab1e9cedd92607b
Samuel Thand <sath2102@student.miun.se>
2023-01-04 16:57:52 +0100

    added debugging statements in POST quiz route, added comments in quiz Schema

--- models/quiz.ts
+++ models/quiz.ts
@@ -23,9 +23,12 @@ interface QuizModel extends Model<IQuiz, {}, IQuizMethods> {
 }
 
 const quizSchema = new Schema<IQuiz, QuizModel, IQuizMethods>({
+  // TODO Problem med att posta en quiz med creator, för att man måste ha ett _ID som refererar till ett
+  // admin dokument creator, hämta det _id:t och ange det när man postar frågan i frontend
   creator: {
     type: Schema.Types.ObjectId,
     ref: 'admin',
+    // type: String,
     required: true
   },
   name: {
