d3e933294cb318c1a2603f262e136cef4d749c2d
Samuel Thand <sath2102@student.miun.se>
2023-01-11 14:28:06 +0100

    Updated express session setup for production/development

--- app.ts
+++ app.ts
@@ -6,10 +6,12 @@ import questionsRoutes from './routes/questions';
 import subjectRoutes from './routes/subjects';
 import quizzesRoutes from './routes/quizzes';
 import adminRoutes from './routes/admins';
-import expressSession from 'express-session';
+import expressSession, { SessionOptions } from 'express-session';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+dotenv.config();
 
 declare module 'express-session' {
   export interface Session {
@@ -17,7 +19,7 @@ declare module 'express-session' {
   }
 }
 
-const allowedOrigins = ['http://localhost:4200', 'https://studenter.miun.se/'];
+const allowedOrigins = ['http://localhost:4200', 'https://studenter.miun.se'];
 
 const corsOptions = {
   credentials: true,
@@ -28,27 +30,33 @@ app.use(cors(corsOptions));
 app.use(express.json()); // Parse incoming JSON payloads with express.json
 app.use(express.urlencoded({ extended: true })); // Parse incoming requests with urlencoded payloads using express.urlencoded
 
-// TODO - sätt upp secure cookies i produktion (Endast med HTTPS) / ej secure i test enligt robbans tips (FÖR HTTP) / sameSite måste sättas till true för att fungera i production!
-app.use(
-  expressSession({
-    secret: 'hemligheten',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      sameSite: true,
-      secure: false,
-      httpOnly: false
-    }
-  })
-);
+const session: SessionOptions = {
+  secret: 'hemligheten',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    sameSite: true,
+    secure: false,
+    httpOnly: true
+  }
+};
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('setting as production');
+  app.set('trust proxy', 1);
+  if (session.cookie !== undefined) {
+    session.cookie.sameSite = 'none';
+    session.cookie.secure = true;
+  }
+}
+
+app.use(expressSession(session));
+
 app.use('/questions', questionsRoutes);
 app.use('/subjects', subjectRoutes);
 app.use('/quizzes', quizzesRoutes);
 app.use('/admins', adminRoutes);
 
-// TODO autentisering med express-session
-
-dotenv.config();
 mongoose
   .connect(process.env.DB_SERVER!)
   .then(() => {
